c988400dc98c2de91d9e7f4d9fe4ae9614ecdfbd
wulf <czh1983@gmail.com>
2024-10-09 14:40:44 -0300

    updates

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,43 +1,13 @@
 "use client";
-import { useState, useEffect } from "react";
-import MarkerIcon from "leaflet/dist/images/marker-icon.png";
-import MarkerShadow from "leaflet/dist/images/marker-shadow.png";
-import L, { ColorGradientConfig } from "leaflet";
+
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer} from "react-leaflet";
 import "leaflet.heat";
 import LocateControl from "./LocateControl";
-import HeatmapLayer from "react-leaflet-heat-layer";
 // import Search from "./search"
 import { Location } from "./Location";
-
-const gradientConfig: ColorGradientConfig = {
-  0: "#0000FF", // Azul oscuro
-  0.1: "#00FFFF", // Azul claro
-  0.3: "#FFFFFF", // Blanco
-  0.5: "#0000FF", // Rojo
-  0.7: "#FFA500", // Naranja
-  1: "#FF0000", // Rojo
-};
-
-interface Event {
-  id: string;
-  name: string;
-  type: string;
-  date: string;
-  time: string;
-  location: {
-    lat: number;
-    lon: number;
-  };
-  photos?: string[];
-  description: string;
-  amount: number;
-  createdAt: string;
-  userId: string;
-  capacity: string;
-  addres: string;
-}
+import Markers from "./Markers";
+import Heatmap from "./Heatmap";
 
 // const addressPoints: AddressPoint[] = [
 //   { lat: -34.5828, lng: -58.4158, value: 0.95 },
@@ -236,32 +206,10 @@ interface Event {
 //   }
 //   ]
 const Map = () => {
-  const [data, setData] = useState<Event[]>([]);
   const formatLatLng = (latlng: { lat: number; lon: number }) => {
     return `${latlng.lat.toFixed(4)}, ${latlng.lon.toFixed(4)}`;
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://i003-eventmap-back.onrender.com/events/all"
-        );
-        console.log(response);
-
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const eventData: Event[] = await response.json();
-        setData(eventData);
-        console.log(eventData);
-      } catch (error) {
-        console.error("Error fetching events:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <MapContainer
       style={{ height: "100vh", width: "100vw" }}
@@ -275,21 +223,9 @@ const Map = () => {
       />
       <Location center={[-34.603851, -58.381775]} />
 
-      {/* Heatmap Layer */}
-      <HeatmapLayer
-        latlngs={data.map((event) => [
-          event.location.lat,
-          event.location.lon,
-          event.amount,
-        ])}
-        radius={40}
-        blur={20}
-        maxZoom={15}
-        max={1}
-        gradient={gradientConfig}
-        minOpacity={0.8}
-      />
-
+      <Heatmap />
+      <Markers />
+      <LocateControl />
       {/* <Search
         center={[-34.603851, -58.381775]}
         zoom={15}
@@ -297,46 +233,6 @@ const Map = () => {
           console.log("Área seleccionada:", bounds);
         }}
       /> */}
-
-      {/* 
-          <Popup>
-            Intensidad: {point.value.toFixed(2)}
-            <br />
-            Ubicacion: {formatLatLng(point)}
-          </Popup>
-        </Marker>
-      ))} */}
-
-      {data.map((event) => (
-        <Marker
-          key={event.id}
-          position={[event.location.lat, event.location.lon]}
-          icon={
-            new L.Icon({
-              iconUrl: MarkerIcon.src,
-              iconRetinaUrl: MarkerIcon.src,
-              iconSize: [25, 41],
-              iconAnchor: [12.5, 41],
-              popupAnchor: [0, -41],
-              shadowUrl: MarkerShadow.src,
-              shadowSize: [41, 41],
-            })
-          }
-        >
-          <Popup>
-            <h3>{event.name}</h3>
-            <p>Type: {event.type}</p>
-            <p>Date: {event.date}</p>
-            <p>Time: {event.time}</p>
-            <p>Capacity: {event.capacity}</p>
-            <p>Description: {event.description}</p>
-            <p>Amount: ${event.amount.toFixed(2)}</p>
-            <p>Address: {event.addres}</p>
-          </Popup>
-        </Marker>
-      ))}
-
-      <LocateControl />
     </MapContainer>
   );
 };
