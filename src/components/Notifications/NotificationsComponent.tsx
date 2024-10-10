353e40e9e06d5d886b938455f19c6ebcea1f5911
Ulises <uliisesrodriguez809@gmail.com>
2024-10-10 15:15:02 -0300

    peticion fetch

--- src/components/Notifications/NotificationsComponent.tsx
+++ src/components/Notifications/NotificationsComponent.tsx
@@ -1,16 +1,27 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 
 export const NotificationsComponent = () => {
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const request = await fetch(`http://localhost:3000/events/all`);
+                const result = await request.json();
+                setEvents(result.filter((event: any) => event.amount >= 0.8));
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
+
+    const [events, setEvents] = useState([]);
+
     return (
         <>
-        {/* no hay context hacete peticion la back 
-        las cards usa el componente cardevnts q se parece mucho
-
-        cardEventList
-        
-        */}
             <div className="py-5 px-4">
                 <Link href={"/"}>
                     <BiArrowBack className="mb-4" size={24} color="black" />
@@ -18,6 +29,7 @@ export const NotificationsComponent = () => {
 
                 <h1 className="font-bold text-2xl text-black">Notificaciones</h1>
                 <div className="flex flex-col items-center gap-4 py-6">
+                    <button onClick={() => console.log(events)}>Obtener eventos</button>
                 </div>
             </div>
         </>
